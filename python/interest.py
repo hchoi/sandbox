640274fbb755d1b9427c6598d1dc85ae611a9090
Hyun Deok, Choi <makdong2@gmail.com>
2013-05-09 01:01:32 +0900

    use format funtcion

--- python/interest.py
+++ python/interest.py
@@ -4,6 +4,7 @@ numyears = 5
 year = 1
 while year <= numyears:
     principal = principal * (1 + rate)
-    #   print year, principal   
-    print "%3d %0.2f" % ( year, principal )   
+    # print year, principal   
+    # print "%3d %0.2f" % ( year, principal )   
+    print "{0:3d} {1:0.2f}".format( year, principal )   
     year += 1
